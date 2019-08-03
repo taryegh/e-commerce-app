b04eb7694d24b36459c071bf26c57c78c698987d
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-03 16:16:21 +0400

    grand update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from Navbar</h3>
-      </div>
-    )
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to='/cart' className='ml-auto'>
+          <button>
+            <i className='icon ion-md-cart'/>  
+          </button>  
+        </Link>
+      </nav>
+    );
   }
 }
