845b18641ac5ee041a0fb19b513e36307dea100e
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-03 20:15:58 +0400

    major update

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
 import { storeProducts } from "../data";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
   state = {
@@ -14,7 +15,13 @@ export default class ProductList extends Component {
         <div className="py-5">
           <div className="container">
             <Title name="our" title="products" />
-            <div className="row" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  console.log(value);
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </React.Fragment>
