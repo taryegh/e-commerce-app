3bcfb0b4030a8e9fc0892cee1af36caa6e876d35
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-03 18:59:13 +0400

    grand update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="" className="navbar-brand" />
         </Link>
+
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
@@ -16,12 +19,25 @@ export default class Navbar extends Component {
             </Link>
           </li>
         </ul>
-        <Link to='/cart' className='ml-auto'>
-          <button>
-            <i className='icon ion-md-cart'/>  
-          </button>  
+
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="icon ion-md-cart" />
+            </span>
+            my cart
+          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
+
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link {
+  color: var(--mainWhite)!important;
+  font-size: 1.3rem;
+  text-transform: capitalize;
+}
+`
