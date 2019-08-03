494c23d8fd6beb72ec65ec159b3458c4e828dbf3
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-03 12:04:54 +0400

    wip

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h3>hello from app</h3>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
