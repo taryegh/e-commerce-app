2098cdd77262b9645c4d22414afc181eb5e989a0
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-04 08:25:50 +0400

    products are now on the page

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
-import { storeProducts } from "../data";
 import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
-  state = {
-    products: storeProducts
-  };
   render() {
-    console.log(this.state.products);
     return (
       <React.Fragment>
         <div className="py-5">
@@ -18,14 +13,15 @@ export default class ProductList extends Component {
             <div className="row">
               <ProductConsumer>
                 {value => {
-                  console.log(value);
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
                 }}
               </ProductConsumer>
             </div>
           </div>
         </div>
       </React.Fragment>
-      // <Product />
     );
   }
 }
