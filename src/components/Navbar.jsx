3784d7d52e210cbb1e4c64cb42c47c44060f11be
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-08 21:38:00 +0400

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import logo from "../logo.svg";
+// import logo from "../logo.svg";
 import styled from "styled-components";
 import { ButtonContainer } from "./Button";
 
