483920b1818445ffb91468673d03c945f5d143dd
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-09 00:53:32 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, HashRouter } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
@@ -11,15 +11,17 @@ import Default from "./components/Default";
 class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={ProductList} />
-          <Route path="/details" component={Details} />
-          <Route path="/cart" component={Cart} />
-          <Route component={Default} />
-        </Switch>
-      </React.Fragment>
+      <HashRouter basename="/">
+        <React.Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={ProductList} />
+            <Route path="/details" component={Details} />
+            <Route path="/cart" component={Cart} />
+            <Route component={Default} />
+          </Switch>
+        </React.Fragment>
+      </HashRouter>
     );
   }
 }
