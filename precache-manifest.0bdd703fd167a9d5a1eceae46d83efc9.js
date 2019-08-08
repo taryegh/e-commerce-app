dcf2673534cf68a7cda141acc63340940f6f9cc0
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-08 21:39:36 +0400

    Updates

---
+++ precache-manifest.0bdd703fd167a9d5a1eceae46d83efc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719d118b5522b27cb81c5febed7cac67",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "5bba01efeb7dd01811f0",
+    "url": "/e-commerce/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "34f53ac9e5b11e4610a1",
+    "url": "/e-commerce/static/css/main.ea28b24c.chunk.css"
+  },
+  {
+    "revision": "5bba01efeb7dd01811f0",
+    "url": "/e-commerce/static/js/2.ea5068b2.chunk.js"
+  },
+  {
+    "revision": "34f53ac9e5b11e4610a1",
+    "url": "/e-commerce/static/js/main.b70b7245.chunk.js"
+  },
+  {
+    "revision": "290256ac1007569aecdb",
+    "url": "/e-commerce/static/js/runtime~main.8575550b.js"
+  }
+]);
