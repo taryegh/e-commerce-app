d3787c28cb0235f43607b1ed858c3280f0bbff0e
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-10 23:11:27 +0400

    Modal added

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -59,6 +59,7 @@ export default class Details extends Component {
                       disabled={inCart ? true : false}
                       onClick={() => {
                         value.addToCart(id);
+                        value.openModal(id);
                       }}
                     >
                       {inCart ? "in Cart" : "add to cart"}
