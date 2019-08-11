d295c74ff3ef083db8209c0964fd8e846631266d
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-11 15:14:40 +0400

    stylings

--- src/components/Cart/EmptyCart.jsx
+++ src/components/Cart/EmptyCart.jsx
@@ -5,6 +5,7 @@ export default function EmptyCart() {
     <div className="row">
       <div className="col-10 my-2 mx-auto text-center text-title">
         <h1>cart is empty</h1>
+        <i className="icon ion-ios-cart cart-big" />
       </div>
     </div>
   );
