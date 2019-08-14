d0f50ae04d13768e25f70acf907fe851cafd15fb
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-14 11:56:13 +0400

    Updates

---
+++ precache-manifest.304752a6d7d8320442ef36dcef43bff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b541ee4779cade6350616e6173140a44",
+    "url": "/e-commerce-react/index.html"
+  },
+  {
+    "revision": "7c2db9a1ca6cc111ac07",
+    "url": "/e-commerce-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d7d529477c1a7c2c2a07",
+    "url": "/e-commerce-react/static/css/main.4f1d15a1.chunk.css"
+  },
+  {
+    "revision": "7c2db9a1ca6cc111ac07",
+    "url": "/e-commerce-react/static/js/2.f189e798.chunk.js"
+  },
+  {
+    "revision": "d7d529477c1a7c2c2a07",
+    "url": "/e-commerce-react/static/js/main.aadef793.chunk.js"
+  },
+  {
+    "revision": "63355b0b88f2e49e0083",
+    "url": "/e-commerce-react/static/js/runtime~main.d83870d1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/e-commerce/precache-manifest.5cd28cc8bed0c0598ec2dd7d8bb14630.js"
+  "/e-commerce-react/precache-manifest.304752a6d7d8320442ef36dcef43bff0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-commerce/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-commerce-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
