bb1c5e474a3ccb257caafe79fffe14cb71bf0de4
taryegh <taryegh@gmail.com>
2019-09-15 10:33:19 +0400

    Updates

--- precache-manifest.1d6eb024cc2166f150f831a0c04ea24d.js
+++ precache-manifest.1d6eb024cc2166f150f831a0c04ea24d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e70aca4a9ff97454ba3057cd3d4df6d",
+    "revision": "9d1326ec8aac6a98139c4f845e7ec395",
     "url": "/e-commerce-react/index.html"
   },
   {
-    "revision": "c1f8f11716290587861f",
+    "revision": "72da84f990e89281055d",
     "url": "/e-commerce-react/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "1ee10ffaf1bb525cb856",
+    "revision": "f72226864c53ea543a12",
     "url": "/e-commerce-react/static/css/main.191897fb.chunk.css"
   },
   {
-    "revision": "c1f8f11716290587861f",
-    "url": "/e-commerce-react/static/js/2.f67c3139.chunk.js"
+    "revision": "72da84f990e89281055d",
+    "url": "/e-commerce-react/static/js/2.9e751579.chunk.js"
   },
   {
-    "revision": "1ee10ffaf1bb525cb856",
-    "url": "/e-commerce-react/static/js/main.94580d34.chunk.js"
+    "revision": "f72226864c53ea543a12",
+    "url": "/e-commerce-react/static/js/main.30c1bec6.chunk.js"
   },
   {
     "revision": "63355b0b88f2e49e0083",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/e-commerce-react/precache-manifest.d915358152ca9718a1c7b0196271c93d.js"
+  "/e-commerce-react/precache-manifest.1d6eb024cc2166f150f831a0c04ea24d.js"
 );
 
 self.addEventListener('message', (event) => {
